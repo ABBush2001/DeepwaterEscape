c342f680e328e18a07b0fdcf7c2abceb66cd0871
Alex Bush <qdf438@my.utsa.edu>
2025-04-01 01:35:49 -0500

    Lots of various fixes/changes

--- Assets/Scripts/Level3Scripts/Platforming/Level3NextTrigger.cs
+++ Assets/Scripts/Level3Scripts/Platforming/Level3NextTrigger.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level2NextLevelTrigger : MonoBehaviour
+public class Level3NextTrigger : MonoBehaviour
 {
     [SerializeField] GameObject mainCamera;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             Debug.Log("Collided!");
 
@@ -27,6 +27,6 @@ public class Level2NextLevelTrigger : MonoBehaviour
     {
         mainCamera.GetComponent<CameraFadeOut>().fadeOut = true;
         yield return new WaitForSeconds(4);
-        SceneManager.LoadScene("Level3Test");
+        SceneManager.LoadScene("4.Arena");
     }
 }
