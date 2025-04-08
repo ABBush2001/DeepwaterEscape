bfc2aa1179283d324e037011712ca5247d33e9aa
ABBush2001 <alex.bush@my.utsa.edu>
2025-04-08 00:25:47 -0500

    Set up survey

--- Assets/UI/Scripts/PauseMenu.cs
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -60,6 +60,7 @@ public class PauseMenu2 : MonoBehaviour
     public void QuitGame()
     {
         Time.timeScale = 1f;
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfwdjz4HT0iWeojGLPPhOp7fo7Z4mVy0J8iz__-lf81F_aDhA/viewform?usp=header");
         Application.Quit();
     }
 
