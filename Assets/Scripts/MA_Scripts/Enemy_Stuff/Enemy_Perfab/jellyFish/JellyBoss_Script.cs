f279c1f15553c227ad540ccd14e56fa341d05552
MArreola78 <amauricio4467@gmail.com>
2025-04-13 22:01:20 -0500

    Jellyfish Jump

--- Assets/Scripts/MA_Scripts/Enemy_Stuff/Enemy_Perfab/jellyFish/JellyBoss_Script.cs
+++ Assets/Scripts/MA_Scripts/Enemy_Stuff/Enemy_Perfab/jellyFish/JellyBoss_Script.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class JellyBoss_Script : MonoBehaviour
 {
 
-    [SerializeField] Vector3 movement;
-    [SerializeField] Vector3 rotation;
+    //[SerializeField] Vector3 movement;
+    //[SerializeField] Vector3 rotation;
 
-    [SerializeField] bool fullCycle;
+    //[SerializeField] bool fullCycle;
 
-    [SerializeField] float completionTime = 3f;
-    [SerializeField] float endPointStallTime = 2f;
+    //[SerializeField] float completionTime = 3f;
+    //[SerializeField] float endPointStallTime = 2f;
 
-    [SerializeField] float endPointDistanceThreshold = 0.05f;
+    //[SerializeField] float endPointDistanceThreshold = 0.05f;
 
-    float dwellTimer = 0f;
-    float t = 0f;
+    //float dwellTimer = 0f;
+    //float t = 0f;
 
-    Vector3 startingPoint;
-    Vector3 targetPoint;
+    //Vector3 startingPoint;
+    //Vector3 targetPoint;
 
 
     public int waveDmg = 999;
@@ -27,57 +27,57 @@ public class JellyBoss_Script : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        targetPoint = (transform.position + movement);
+        //targetPoint = (transform.position + movement);
 
-        if (fullCycle)
-        {
-            transform.position = (transform.position - movement);
-        }
+        //if (fullCycle)
+        //{
+        //    transform.position = (transform.position - movement);
+        //}
 
-        startingPoint = transform.position;
+        //startingPoint = transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if(Vector3.Distance(transform.position, targetPoint) <= endPointDistanceThreshold)
-        {
-            dwellTimer += Time.deltaTime;
+        //if(Vector3.Distance(transform.position, targetPoint) <= endPointDistanceThreshold)
+        //{
+        //    dwellTimer += Time.deltaTime;
 
-            if (dwellTimer >= endPointStallTime)
-            {
-                Vector3 temp = targetPoint;
-                targetPoint = startingPoint;
-                startingPoint = temp;
+        //    if (dwellTimer >= endPointStallTime)
+        //    {
+        //        Vector3 temp = targetPoint;
+        //        targetPoint = startingPoint;
+        //        startingPoint = temp;
 
-                t = 0f;
-                dwellTimer = 0f;
-            }
-        }
+        //        t = 0f;
+        //        dwellTimer = 0f;
+        //    }
+        //}
 
-        t += (Time.deltaTime / completionTime);
+        //t += (Time.deltaTime / completionTime);
 
-        transform.position = Vector3.Lerp(startingPoint, targetPoint, t);
-        //transform.position = Vector3.Lerp(transform.position, targetPoint, Time.deltaTime * speed);
+        //transform.position = Vector3.Lerp(startingPoint, targetPoint, t);
+        ////transform.position = Vector3.Lerp(transform.position, targetPoint, Time.deltaTime * speed);
 
-        transform.Rotate(rotation * Time.deltaTime);
+        //transform.Rotate(rotation * Time.deltaTime);
     }
 
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Vector3 initialPosition = Application.isPlaying ? transform.position : transform.position - movement;
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Vector3 initialPosition = Application.isPlaying ? transform.position : transform.position - movement;
 
-        if (fullCycle)
-        {
-            Gizmos.DrawLine(initialPosition - movement, initialPosition + movement);
-        }
-        else
-        {
-            Gizmos.DrawLine(initialPosition, initialPosition + movement);
-        }
-    }
+    //    if (fullCycle)
+    //    {
+    //        Gizmos.DrawLine(initialPosition - movement, initialPosition + movement);
+    //    }
+    //    else
+    //    {
+    //        Gizmos.DrawLine(initialPosition, initialPosition + movement);
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
