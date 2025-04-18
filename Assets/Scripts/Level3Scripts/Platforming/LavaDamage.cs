01bb23d2f32c60eb63924d3f44b4c2f985ad7a46
KillerMeme3001 <hayden.burke@outlook.com>
2025-04-17 21:51:39 -0500

    Bug Fixes, Lava Fix

--- Assets/Scripts/Level3Scripts/Platforming/LavaDamage.cs
+++ Assets/Scripts/Level3Scripts/Platforming/LavaDamage.cs
@@ -13,18 +13,20 @@ public class LavaDamage : MonoBehaviour
     public Camera mainCamera;
 
     //check for collision with player and lava
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))
         {
             StartCoroutine(reloadScene());
+            Debug.Log("Lava Fall!!");
         }
     }
 
     //reload the level
     IEnumerator reloadScene()
     {
-        mainCamera.GetComponent<CameraFadeOut>().fadeOut = true;
+        //mainCamera.GetComponent<CameraFadeOut>().fadeOut = true;
+        mainCamera.transform.SetParent(null, true);
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
