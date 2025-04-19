28d8c34a839f38af90655108835d06e6b4076438
MArreola78 <amauricio4467@gmail.com>
2025-04-18 22:37:58 -0500

    jellyboss chnge scene

--- Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/jellyFish/JellyBoss_Script.cs
+++ Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/jellyFish/JellyBoss_Script.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JellyBoss_Script : MonoBehaviour
 {
@@ -22,7 +23,9 @@ public class JellyBoss_Script : MonoBehaviour
     //Vector3 targetPoint;
 
 
-    public int waveDmg = 999;
+    //public int waveDmg = 999;
+
+    public string sceneToLoad;
 
     // Start is called before the first frame update
     private void Start()
@@ -81,13 +84,6 @@ public class JellyBoss_Script : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            Player_Health playerHealth = other.GetComponent<Player_Health>();
-            if (playerHealth != null)
-            {
-                playerHealth.TakeDamage(waveDmg);
-            }
-        }
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
