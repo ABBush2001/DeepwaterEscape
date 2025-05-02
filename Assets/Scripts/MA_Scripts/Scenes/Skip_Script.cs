65bd97ddf5fbb8997bd646c2395b74eb38d28ca3
MArreola78 <amauricio4467@gmail.com>
2025-05-02 14:38:07 -0500

    press e to skip cutscene

--- Assets/Scripts/MA_Scripts/Scenes/Skip_Script.cs
+++ Assets/Scripts/MA_Scripts/Scenes/Skip_Script.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Video;
 
 public class Skip_Script : MonoBehaviour
 {
+    public TMP_Text textOff;
+    public VideoPlayer videoPlayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +20,16 @@ public class Skip_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LoadScene();
+            textOff.gameObject.SetActive(false);
+        }
     }
 
     public void LoadScene()
     {
-        SceneManager.LoadScene("1.Submarine");
+        //SceneManager.LoadScene("1.Submarine");
+        videoPlayer.Stop();
     }
 }
