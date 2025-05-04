d5b2401e5bb2602daa876f5324184d4e8bbd8812
Hoxatron <derpman8000@gmail.com>
2025-05-04 02:50:04 -0500

    Fix VolumeLoader + Set Company Name to Cracked Conch

--- Assets/Scripts/General/VolumeLoader.cs
+++ Assets/Scripts/General/VolumeLoader.cs
@@ -5,11 +5,24 @@ public class VolumeLoader : MonoBehaviour
 {
     [SerializeField][Tooltip("The master audio mixer.")] private AudioMixer masterMixer;
 
+    private const float minVol = -80f; // Unity's mute level
+
     // Start is called before the first frame update
     void Start()
     {
-        masterMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
-        masterMixer.SetFloat("BGMVolume", PlayerPrefs.GetFloat("BGMVolume"));
-        masterMixer.SetFloat("SFXVolume", PlayerPrefs.GetFloat("SFXVolume"));
+        // Retrieve values
+        float masterVol = PlayerPrefs.GetFloat("MasterVolume");
+        float BGMVol = PlayerPrefs.GetFloat("BGMVolume");
+        float SFXVol = PlayerPrefs.GetFloat("SFXVolume");
+
+        // If values are above 0, convert them to suitable audio mixer values.
+        masterVol = PlayerPrefs.GetFloat("MasterVolume") > 0 ? Mathf.Log10(masterVol) * 20 : minVol;
+        BGMVol = PlayerPrefs.GetFloat("BGMVolume") > 0 ? Mathf.Log10(BGMVol) * 20 : minVol;
+        SFXVol = PlayerPrefs.GetFloat("SFXVolume") > 0 ? Mathf.Log10(SFXVol) * 20 : minVol;
+
+        // Set the mixer volume.
+        masterMixer.SetFloat("MasterVolume", masterVol);
+        masterMixer.SetFloat("BGMVolume", BGMVol);
+        masterMixer.SetFloat("SFXVolume", SFXVol);
     }
 }
