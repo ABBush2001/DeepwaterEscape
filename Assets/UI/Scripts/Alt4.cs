714864ad6324b0d8b17bf2545c0d426f390dfcec
Hoxatron <derpman8000@gmail.com>
2025-05-06 01:06:33 -0500

    Queen Flash Fix + Noise

--- Assets/UI/Scripts/Alt4.cs
+++ Assets/UI/Scripts/Alt4.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class Alt4 : MonoBehaviour
 {
-    void Update()
-    {
-        // this won't intercept an alt-f4
-        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.F4))
-        {
-            // Feedback form
-            //Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfwdjz4HT0iWeojGLPPhOp7fo7Z4mVy0J8iz__-lf81F_aDhA/viewform?usp=header");
-            //Application.Quit();
-        }
-    }
+    //void Update()
+    //{
+    //    // this won't intercept an alt-f4
+    //    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.F4))
+    //    {
+    //        // Feedback form
+    //        //Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfwdjz4HT0iWeojGLPPhOp7fo7Z4mVy0J8iz__-lf81F_aDhA/viewform?usp=header");
+    //        //Application.Quit();
+    //    }
+    //}
 
     //*this* will detect the quit command
     void OnApplicationQuit()
     {
-        
+
     }
 }
