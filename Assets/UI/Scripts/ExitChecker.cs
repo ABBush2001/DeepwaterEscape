ded0fbc0991121d39897fc212d4f4799996ceeb1
Hoxatron <derpman8000@gmail.com>
2025-05-13 04:23:09 -0500

    Unsuccessful Queen Death Animation Fix
    Patchwork attempts to make the queen death animation play out

--- Assets/UI/Scripts/ExitChecker.cs
+++ Assets/UI/Scripts/ExitChecker.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitChecker : MonoBehaviour
 {
+    public ExitHandler progressionCheck;
     // Start is called before the first frame update
+
     void Start()
     {
-       
+        PlayerPrefs.SetInt("HasJellied", 1);
+        progressionCheck.hasJellied = true;
     }
 }
