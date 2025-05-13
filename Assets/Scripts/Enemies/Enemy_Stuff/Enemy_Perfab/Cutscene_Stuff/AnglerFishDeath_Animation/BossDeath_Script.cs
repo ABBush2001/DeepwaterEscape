15a7b86e03d4ad0f8e9d492955c2dd78f148c233
Hoxatron <derpman8000@gmail.com>
2025-05-13 03:22:55 -0500

    Revert "Merge remote-tracking branch 'origin/MA_Branch' into WyattBBranch"
    This reverts commit 431f8bc58228a0c7b9dfe35179ded93044661318, reversing
    changes made to f3a1f9c3b970a1153732c118e91e3696ccd4a868.

--- Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/Cutscene_Stuff/AnglerFishDeath_Animation/BossDeath_Script.cs
+++ Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/Cutscene_Stuff/AnglerFishDeath_Animation/BossDeath_Script.cs
@@ -5,43 +5,41 @@ using UnityEngine;
 public class BossDeath_Script : MonoBehaviour
 {
     // Main Camera and the death camera
-    //public Camera mainCamera;
-    //public Camera deathCamera;
+    public Camera mainCamera;
+    public Camera deathCamera;
 
     // Disable health and title
-    //public GameObject healthBarUI;
-    //public GameObject bossTitle;
+    public GameObject healthBarUI;
+    public GameObject bossTitle;
 
     // Animator components
     public Animator bossAnimator;
-    //public Animator deathCameraAnimator;
+    public Animator deathCameraAnimator;
 
 
     public GameObject cutscene2;
 
     public void TriggerBossDeath()
     {
-        //mainCamera.enabled = false;
-        //deathCamera.enabled = true;
+        mainCamera.enabled = false;
+        deathCamera.enabled = true;
 
         bossAnimator.SetTrigger("Death");
-        bossAnimator.Play("AFDeath_AMN");
-        //deathCameraAnimator.SetTrigger("Activate");
+        deathCameraAnimator.SetTrigger("Activate");
 
-        //healthBarUI.SetActive(false);
-        //bossTitle.SetActive(false);
+        healthBarUI.SetActive(false);
+        bossTitle.SetActive(false);
 
         StartCoroutine(ActivateAnimation());
     }
 
     private IEnumerator ActivateAnimation()
     {
-        //bossAnimator.Play("AFDeath_AMN");
         yield return new WaitForSeconds(bossAnimator.GetCurrentAnimatorStateInfo(0).length);
 
-       // deathCamera.enabled = false;
-       // mainCamera.enabled = true;
+        deathCamera.enabled = false;
+        mainCamera.enabled = true;
 
-        //cutscene2.GetComponent<ClosingBossCutscene>().BeginCutscene();
+        cutscene2.GetComponent<ClosingBossCutscene>().BeginCutscene();
     }
 }
