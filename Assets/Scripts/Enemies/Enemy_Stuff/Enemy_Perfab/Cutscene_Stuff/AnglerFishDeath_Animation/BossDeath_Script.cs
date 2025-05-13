a4ec9fbf53c6cda96b7190db89aaa9db7a53cd70
MArreola78 <amauricio4467@gmail.com>
2025-05-13 02:40:04 -0500

    animation queen

--- Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/Cutscene_Stuff/AnglerFishDeath_Animation/BossDeath_Script.cs
+++ Assets/Scripts/Enemies/Enemy_Stuff/Enemy_Perfab/Cutscene_Stuff/AnglerFishDeath_Animation/BossDeath_Script.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class BossDeath_Script : MonoBehaviour
 {
     // Main Camera and the death camera
-    public Camera mainCamera;
-    public Camera deathCamera;
+    //public Camera mainCamera;
+    //public Camera deathCamera;
 
     // Disable health and title
-    public GameObject healthBarUI;
-    public GameObject bossTitle;
+    //public GameObject healthBarUI;
+    //public GameObject bossTitle;
 
     // Animator components
     public Animator bossAnimator;
-    public Animator deathCameraAnimator;
+    //public Animator deathCameraAnimator;
 
 
     public GameObject cutscene2;
 
     public void TriggerBossDeath()
     {
-        mainCamera.enabled = false;
-        deathCamera.enabled = true;
+        //mainCamera.enabled = false;
+        //deathCamera.enabled = true;
 
         bossAnimator.SetTrigger("Death");
-        deathCameraAnimator.SetTrigger("Activate");
+        //deathCameraAnimator.SetTrigger("Activate");
 
-        healthBarUI.SetActive(false);
-        bossTitle.SetActive(false);
+        //healthBarUI.SetActive(false);
+        //bossTitle.SetActive(false);
 
         StartCoroutine(ActivateAnimation());
     }
@@ -37,8 +37,8 @@ public class BossDeath_Script : MonoBehaviour
     {
         yield return new WaitForSeconds(bossAnimator.GetCurrentAnimatorStateInfo(0).length);
 
-        deathCamera.enabled = false;
-        mainCamera.enabled = true;
+       // deathCamera.enabled = false;
+       // mainCamera.enabled = true;
 
         cutscene2.GetComponent<ClosingBossCutscene>().BeginCutscene();
     }
